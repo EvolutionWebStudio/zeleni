3bac74538642c4f4704dd331f4276ed088cf72ab
i.golub <goolub.igor@gmail.com>
2014-02-22 22:37:29 +0100

    added galery

---
+++ protected/views/image/_form.php
@@ -0,0 +1,58 @@
+<?php
+/* @var $this ImageController */
+/* @var $model Image */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'image-form',
+	// Please note: When you enable ajax validation, make sure the corresponding
+	// controller action is handling ajax validation correctly.
+	// There is a call to performAjaxValidation() commented in generated controller code.
+	// See class documentation of CActiveForm for details on this.
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'title'); ?>
+		<?php echo $form->textField($model,'title',array('size'=>45,'maxlength'=>45)); ?>
+		<?php echo $form->error($model,'title'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'link'); ?>
+		<?php echo $form->textField($model,'link',array('size'=>45,'maxlength'=>45)); ?>
+		<?php echo $form->error($model,'link'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'album_id'); ?>
+		<?php echo $form->textField($model,'album_id'); ?>
+		<?php echo $form->error($model,'album_id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'caption'); ?>
+		<?php echo $form->textField($model,'caption',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->error($model,'caption'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'number'); ?>
+		<?php echo $form->textField($model,'number'); ?>
+		<?php echo $form->error($model,'number'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
---
+++ protected/views/image/_view.php
@@ -0,0 +1,33 @@
+<?php
+/* @var $this ImageController */
+/* @var $data Image */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('title')); ?>:</b>
+	<?php echo CHtml::encode($data->title); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('link')); ?>:</b>
+	<?php echo CHtml::encode($data->link); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('album_id')); ?>:</b>
+	<?php echo CHtml::encode($data->album_id); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('caption')); ?>:</b>
+	<?php echo CHtml::encode($data->caption); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('number')); ?>:</b>
+	<?php echo CHtml::encode($data->number); ?>
+	<br />
+
+
+</div>
