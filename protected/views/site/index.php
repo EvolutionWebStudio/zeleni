2597bf998ca71066c7454c6d0a373cecbf6fa6f5
Ilja Tešić <ilija.tesic@gmail.com>
2014-03-11 10:25:19 +0100

    promjenjeno ime foldera sa slikama za clanak

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -4,49 +4,38 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 
-<?php foreach($slides as $slide): ?>
-<?php echo $slide->title; ?>
-<?php echo $slide->link; ?>
-<?php echo $slide->image; ?>
-<?php echo $slide->content; ?>
-<?php echo $slide->lang; ?>
-<?php echo $slide->number; ?>
-<?php echo $slide->color; ?>
-<?php endforeach; ?>
-
 <div class="slider">
     <div class="bar green">
         <div class="row collapse wide main-banner">
+            <div class="clearfix banner-line top"></div>
             <div class="logo-image"></div>
 
-            <article class="caption-wrapper columns">
-                <hgroup>
-                    <h1>Jabuka</h1>
-                    <h2>iz aranđelovačkih voćnjaka</h2>
-                </hgroup>
+            <?php foreach($slides as $slide): ?>
+                <div>
+                    <div class="caption-wrapper columns">
+                        <article>
+                            <?php echo $slide->content; ?>
+                        </article>
+                    </div>
 
-                <p>Čuvamo svoje proizvode u kontrolisanim temperaturnim i atmosferskim uslovima radi
-                    održavanja kvaliteta i svežine tokom čitave godine.</p>
-            </article>
+                    <div class="image-wrapper wider-image columns">
+                        <img src="<?php echo Yii::app()->request->baseUrl; ?>/img/slider/<?php echo $slide->image;?>" alt="Greeny tresnja"/>
+                    </div>
+                </div>
+            <?php endforeach; ?>
 
-            <div class="image-wrapper wider-image columns">
-                <img src="<?php echo Yii::app()->request->baseUrl; ?>/img/slider/greeny-tresnja-slide.png" alt="Greeny tresnja"/>
-            </div>
-            <div class="banner-bottom-line"></div>
+            <div class="clearfix banner-line bottom"></div>
         </div>
-        <div class="clearfix banner-bottom-line"></div>
+
     </div>
 
     <div class="bar gray">
         <div class="row wide">
             <nav class="slider-menu">
                 <ul class="clearfix">
-                    <li class="large-2 columns"><a href="#"><span class="slide-number">01</span> Garancija kvaliteta <span class="link-arrow">&gt;</span></a></li>
-                    <li class="large-2 columns"><a href="#"><span class="slide-number">02</span> Greeny EveryDay <span class="link-arrow">&gt;</span></a></li>
-                    <li class="large-2 columns active"><a href="#"><span class="slide-number">03</span> Sveže voće <span class="link-arrow">&gt;</span></a></li>
-                    <li class="large-2 columns"><a href="#"><span class="slide-number">04</span> Trešnja <span class="link-arrow">&gt;</span></a></li>
-                    <li class="large-2 columns"><a href="#"><span class="slide-number">05</span> Jagoda <span class="link-arrow">&gt;</span></a></li>
-                    <li class="large-2 columns"><a href="#"><span class="slide-number">06</span> Jabuka <span class="link-arrow">&gt;</span></a></li>
+                    <?php foreach($slides as $slide): ?>
+                        <li class="large-2 columns"><a href="<?php echo $slide->link; ?>"><span class="slide-number">0<?php echo $slide->number; ?></span> <?php echo $slide->title; ?> <span class="link-arrow">&gt;</span></a></li>
+                    <?php endforeach; ?>
                 </ul>
             </nav>
         </div>
