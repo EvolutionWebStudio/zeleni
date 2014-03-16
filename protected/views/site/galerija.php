5aa4c4c86ed2363b54d868bdd855696c67559540
Ilja Tešić <ilija.tesic@gmail.com>
2014-03-16 18:12:18 +0100

    ubacene slike u galeriju

--- protected/views/site/galerija.php
+++ protected/views/site/galerija.php
@@ -1,28 +1,14 @@
-<?php foreach($albums as $album): ?>
-	<?php echo $album->name; ?>
-	<?php echo $album->alias; ?>
-	<?php echo $album->image; ?>
-<?php endforeach; ?>
-<?php foreach($images as $image): ?>
-	<?php echo $image->title; ?>
-	<?php echo $image->caption; ?>
-<?php endforeach; ?>
 <div class="bar green">
     <div class="row collapse wide main-gallery">
         <div class="clearfix banner-line top"></div>
         <div class="logo-image"></div>
 
         <ul class="gallery-bxslider">
-            <li>
-                <img src="<?php echo Yii::app()->request->baseUrl; ?>/img/gallery/tresnja/sweet cherries Greeny Arandjelovac_1-1220.jpg" alt="Greeny tresnja" title="Ovo je neki caption"/>
-            </li>
-            <li>
-                <img src="<?php echo Yii::app()->request->baseUrl; ?>/img/gallery/tresnja/sweet%20cherries%20Greeny%20Arandjelovac_1-1220.jpg" alt="Greeny tresnja"/>
-            </li>
-            <li>
-                <img src="<?php echo Yii::app()->request->baseUrl; ?>/img/gallery/tresnja/sweet%20cherry%20tree%20Greeny%20Arandjelovac-1220.jpg" alt="Greeny tresnja"/>
-            </li>
-
+            <?php foreach($images as $image): ?>
+                <li>
+                    <img src="<?php echo Yii::app()->request->baseUrl . '/img/gallery/' . $image->album->alias . '/' . $image->title; ?>" alt="<?php echo $image->caption; ?>" title="<?php echo $image->caption; ?>"/>
+                </li>
+            <?php endforeach; ?>
         </ul>
         <div class="clearfix banner-line bottom"></div>
     </div>
@@ -31,55 +17,15 @@
 <div class="bar gray">
     <div class="row collapse wide gallery-albums">
         <ul class="clearfix albums-bxslider">
-            <li>
-                <a href="#">
-                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/img/gallery/albums/greeny-thumb-jabuke.jpg" alt="Greeny tresnja"/>
-                <span class="thumb-overlay"></span>
-                    <span class="album-name">Skladistenje</span>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/img/gallery/albums/greeny-thumb-betonski-stubovi.jpg" alt="Greeny tresnja"/>
-                <span class="thumb-overlay"></span>
-                    <span class="album-name">Skladistenje</span>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/img/gallery/albums/greeny-thumb-proizvodnja.jpg" alt="Greeny tresnja"/>
-                <span class="thumb-overlay"></span>
-                    <span class="album-name">Skladistenje</span>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/img/gallery/albums/greeny-thumb-sortiranje.jpg" alt="Greeny tresnja"/>
-                <span class="thumb-overlay"></span>
-                    <span class="album-name">Skladistenje</span>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/img/gallery/albums/greeny-thumb-skladistenje.jpg" alt="Greeny tresnja"/>
+            <?php foreach($albums as $album): ?>
+                <li>
+                    <a href="<?php echo Yii::app()->request->baseUrl . '/galerija-slika/'.$album->alias; ?>">
+                        <img src="<?php echo Yii::app()->request->baseUrl . '/img/gallery/albums/' . $album->image; ?>" alt="<?php echo $album->name; ?>" title="<?php echo $album->name; ?>"/>
                     <span class="thumb-overlay"></span>
-                    <span class="album-name">Skladistenje</span>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/img/gallery/albums/greeny-thumb-skladistenje.jpg" alt="Greeny tresnja"/>
-                    <span class="thumb-overlay"></span>
-                    <span class="album-name">Skladistenje</span>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/img/gallery/albums/greeny-thumb-skladistenje.jpg" alt="Greeny tresnja"/>
-                    <span class="thumb-overlay"></span>
-                    <span class="album-name">Skladistenje</span>
-                </a>
-            </li>
+                        <span class="album-name"><?php echo $album->name; ?></span>
+                    </a>
+                </li>
+            <?php endforeach; ?>
         </ul>
 
         <div class="albums-navigation clearfix">
