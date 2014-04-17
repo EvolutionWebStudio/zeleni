3d64b3657d14ccccee089af2a5debbff3868bd88
i.golub <goolub.igor@gmail.com>
2014-04-17 10:40:18 +0200

    baza i popravljea mapa

--- protected/views/site/_location_map.php
+++ protected/views/site/_location_map.php
@@ -1,5 +1,5 @@
 <?php Yii::app()->clientScript->registerScriptFile('http://maps.googleapis.com/maps/api/js?sensor=true', CClientScript::POS_END); ?>
-<?php Yii::app()->clientScript->registerScriptFile('/js/map.js', CClientScript::POS_END);?>
+<?php Yii::app()->clientScript->registerScriptFile(Yii::app()->request->baseUrl.'/js/map.js', CClientScript::POS_END);?>
 
 <div class="small-12 columns">
     <div class="main-map panel2" id="map"></div>
